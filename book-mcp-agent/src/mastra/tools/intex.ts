7bfa412fd58a8f856043245b1360eb1de8dc7983
busraozer <busra9345@gmail.com>
2025-05-28 18:04:44 +0300

    Değişiklikler yapıldı

--- book-mcp-agent/src/mastra/tools/intex.ts
+++ book-mcp-agent/src/mastra/tools/intex.ts
@@ -5,10 +5,9 @@ import { createSmitheryUrl } from "@smithery/sdk/shared/config.js";
 
 const config = {};
 export const mcps = await new MCPClient({
-  servers: {
-    gameTrend: {
-      url: createSmitheryUrl("https://server.smithery.ai/@Busra-ozer/book-search-mcp/mcp", { config, apiKey: process.env.SMITHERY_API_KEY ,profile:"skinny-clam-p5YUwk"}) ,
-      
+    servers: {
+        gameTrend: {
+            url: createSmitheryUrl("https://server.smithery.ai/@Busra-ozer/book-search-mcp/mcp", { config, apiKey: process.env.SMITHERY_API_KEY, profile: "skinny-clam-p5YUwk" }),
+        }
     }
-  }
 });
